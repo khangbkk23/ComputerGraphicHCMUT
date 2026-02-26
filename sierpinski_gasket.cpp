31280fc16d18c3d3bb305da9c315b7943421155c
khangbkk23 <khang.buitranduycse@hcmut.edu.vn>
2026-02-26 09:49:08 +0700

    update recursive method of sierpinski_gasket

--- sierpinski_gasket.cpp
+++ sierpinski_gasket.cpp
@@ -9,31 +9,71 @@ void init() {
 
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(0.0, 50.0, 0.0, 50.0, -1.0, 1.0);
+    glOrtho(0.0, 75.0, 0.0, 75.0, -1.0, 1.0);
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 }
 
-void display() {
-    GLfloat vertices[3][2] = {
-        {0.0, 0.0},
-        {25.0, 50.0},
-        {50.0, 0.0}
-    };
+typedef float point2[2];
 
-    GLfloat p[2] = {7.5, 5.0};
-    int j, k;
+// void display() {
+//     GLfloat vertices[3][2] = {
+//         {0.0, 0.0},
+//         {25.0, 50.0},
+//         {50.0, 0.0}
+//     };
 
-    glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_POINTS);
-        for (k = 0; k < 500000; ++k) {
-            j = rand() % 3;
-            p[0] = (p[0] + vertices[j][0]) / 2.0;
-            p[1] = (p[1] + vertices[j][1]) / 2.0;
-            glVertex2fv(p);
-        }
+//     GLfloat p[2] = {7.5, 5.0};
+//     int j, k;
+
+//     glClear(GL_COLOR_BUFFER_BIT);
+//     glBegin(GL_POINTS);
+//         for (k = 0; k < 500000; ++k) {
+//             j = rand() % 3;
+//             p[0] = (p[0] + vertices[j][0]) / 2.0;
+//             p[1] = (p[1] + vertices[j][1]) / 2.0;
+//             glVertex2fv(p);
+//         }
+//     glEnd();
+//     glFlush();
+// }
+
+void triangle(point2 a, point2 b, point2 c) {
+    glBegin(GL_TRIANGLES);
+        glVertex2fv(a);
+        glVertex2fv(b);
+        glVertex2fv(c);
     glEnd();
+}
+
+void divide_triangle(point2 a, point2 b, point2 c, int n) {
+    if(n == 0)
+        triangle(a,b,c);
+
+    else {
+        point2 ab, ac, bc;
+
+        for (int i = 0; i < 2; ++i) {
+            ab[i] = (a[i] + b[i]) / 2;
+            ac[i] = (a[i] + c[i]) / 2;
+            bc[i] = (b[i] + c[i]) / 2;
+        }
+
+        divide_triangle(a, ab, ac, n - 1);
+        divide_triangle(c, ac, bc, n - 1);
+        divide_triangle(b, bc, ab, n - 1);
+        }
+}
+
+void display() {
+    point2 vertices[3] = {
+    {0.0, 0.0},
+    {25.0, 50.0},
+    {50.0, 0.0}
+    };
+    glClear(GL_COLOR_BUFFER_BIT);
+    divide_triangle(vertices[0], vertices[1], vertices[2], 5);
     glFlush();
 }
 
